98fbda33856ba0ae454c341b03b0811ca3f51f6a
Lachlan Moore <lachlan@moore.moe>
2025-01-04 22:52:03 +1100

    change to pycord, templated structure for 5 slash commands

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 A discord bot that allows for messages to be created, edited and modified by anyone in which the message exists using commands
 
 ## Commands
-- Create Post
-- Modify Post
-- Append Post
+- Create Post (/createnote)
+- Modify Post (/editnote NUMBER CONTENT)
+- Append Post (/appendnote NUMBER CONTENT)
+- Delete Line (/notedel NUMBER LINENUMBER)
+
+(Note: deleting notes are not currently supported, and should be deleted manually by a server admin)
+FUTURE: /notedelete NUMBER --> checks if you wrote the note and deletes if true
+
+
+#### Note Syntax
+All notes start with:
+Note Title - #XXXX
+
+
+@lacln --> hello@lachl.au
---
+++ main.py
@@ -0,0 +1,114 @@
+## PYTHON MESSAGING BOT
+#
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+
+load_dotenv(override=True)
+token = os.environ['CLIENT_TOKEN']
+# print(token)
+
+# # #Intents --> what the bot intends to receive from the API
+# # intents = Intents.default()
+# # intents.message_content = True
+# # client = Client(intents=intents)
+
+# # #Message Functionality
+# # async def send_message(message, user_message):
+# #     if not user_message:
+# #         print('message not sent user message not filled')
+# #         return
+    
+# #     # := same as isprivate = ... then check if isprivate
+# #     if is_private := user_message[0] == '?':
+# #         user_message = user_message[1:]
+
+# #     #try to get the responce
+# #     try:
+# #         response = get_response(user_message)
+# #         if is_private:
+# #             await message.author.send(response)
+# #         else:
+# #             await message.channel.send(response)
+# #         # await message.author.send(response) if is_private else message.channel.send(response)
+# #     except Exception as e:
+# #         print(e)
+
+
+
+# # def get_response(user_input):
+# #     command = user_input.lower()
+
+# #     if command == "":
+# #         return "A command would have been nice"
+# #     if command == "createnote":
+# #         return "OK"
+# #     else:
+# #         return "Command not found"
+    
+
+# # #STARTUP
+# # @client.event
+# # async def on_ready():
+# #     print(str(client.user) + " is now running")
+
+
+# # #INCOMING MESSAGES
+# # @client.event
+# # async def on_message(message):
+# #     if message.author == client.user:
+# #         return
+# #     # username = str(message.author)
+# #     user_message = message.content
+# #     # channel = str(message.channel)
+
+# #     # print()
+# #     await send_message(message, user_message)
+
+# # #MAIN
+# # if __name__ == "__main__":
+# #     client.run(token=token)
+
+bot = commands.Bot()
+
+@bot.slash_command(
+    name="note_create"
+)
+
+@bot.slash_command(
+    name="note_edit"
+)
+
+@bot.slash_command(
+    name="note_append"
+)
+
+@bot.slash_command(
+    name="note_delete_lines"
+)
+
+@bot.slash_command(
+    name="note_delete"
+)
+
+async def note_create(ctx):
+    await ctx.respond("note should be created")
+
+async def note_edit(ctx):
+    await ctx.respond("note should be edited")
+
+async def note_append(ctx):
+    await ctx.respond("note should be edited")
+
+async def note_delete_lines(ctx):
+    await ctx.respond("note should have lines deleted")
+
+async def note_delete(ctx):
+    await ctx.respond("note should be deleted")
+
+#MAIN
+if __name__ == "__main__":
+    bot.run(token)
+    print("The bot is running...")
