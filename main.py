57a0ecf765eb14455fb02a5e918869d62f43ef3c
Lachlan Moore <lachlan@moore.moe>
2025-01-05 01:34:41 +1100

    added options for slash commands and pointers

--- main.py
+++ main.py
@@ -1,78 +1,31 @@
 ## PYTHON MESSAGING BOT
 #
 import os
+import json
 from dotenv import load_dotenv
 import discord
+from discord import option
 from discord.ext import commands
-
 
 load_dotenv(override=True)
 token = os.environ['CLIENT_TOKEN']
 channels = os.getenv("GUILD_IDS")
 # print(token)
 
-# # #Intents --> what the bot intends to receive from the API
-# # intents = Intents.default()
-# # intents.message_content = True
-# # client = Client(intents=intents)
-
-# # #Message Functionality
-# # async def send_message(message, user_message):
-# #     if not user_message:
-# #         print('message not sent user message not filled')
-# #         return
-    
-# #     # := same as isprivate = ... then check if isprivate
-# #     if is_private := user_message[0] == '?':
-# #         user_message = user_message[1:]
-
-# #     #try to get the responce
-# #     try:
-# #         response = get_response(user_message)
-# #         if is_private:
-# #             await message.author.send(response)
-# #         else:
-# #             await message.channel.send(response)
-# #         # await message.author.send(response) if is_private else message.channel.send(response)
-# #     except Exception as e:
-# #         print(e)
-
-
-
-# # def get_response(user_input):
-# #     command = user_input.lower()
-
-# #     if command == "":
-# #         return "A command would have been nice"
-# #     if command == "createnote":
-# #         return "OK"
-# #     else:
-# #         return "Command not found"
-    
-
-# # #STARTUP
-# # @client.event
-# # async def on_ready():
-# #     print(str(client.user) + " is now running")
-
-
-# # #INCOMING MESSAGES
-# # @client.event
-# # async def on_message(message):
-# #     if message.author == client.user:
-# #         return
-# #     # username = str(message.author)
-# #     user_message = message.content
-# #     # channel = str(message.channel)
-
-# #     # print()
-# #     await send_message(message, user_message)
-
-# # #MAIN
-# # if __name__ == "__main__":
-# #     client.run(token=token)
-
 bot = commands.Bot()
+
+
+## BACKEND.JSON IN FORMAT
+"""
+messages:{[ARR]}
+
+ARR OF
+{
+messageid:0000000
+creator:username
+contents:TEXT HERE
+}
+"""
 
 
 """
@@ -81,46 +34,90 @@ NEXT UP
 get args for the slash command
 figure out who wrote the command (author)
 get a message id number of messages sent/recieved
+
+
+BACKEND IN JSON FORMAT IN EXTERNAL FLAT FILE
 """
 
 @bot.slash_command(
     name="note_create",
+    description="Create a new note",
     guild_ids=[channels]
 )
+@option(
+    "content",
+    description="note contents"
+)
+async def note_create(ctx, content):
+    #Ensure first line of file is auto added
+    #NOTE_NUM: 00000000 -- CREATED BY: USERNAME
+    await ctx.respond(f"note should be created and content is {content}")
+
 
 @bot.slash_command(
     name="note_edit",
+    description="Edit an existing note",
     guild_ids=[channels]
 )
-
-@bot.slash_command(
-    name="note_append",
-    guild_ids=[channels]
+@option(
+    "message_id",
+    description="message_id (first line of message)"
 )
-
-@bot.slash_command(
-    name="note_delete_lines",
-    guild_ids=[channels]
+@option(
+    "content",
+    description="new note contents"
 )
-
-@bot.slash_command(
-    name="note_delete",
-    guild_ids=[channels]
-)
-
-async def note_create(ctx):
-    await ctx.respond("note should be created")
-
 async def note_edit(ctx):
     await ctx.respond("note should be edited")
 
+
+@bot.slash_command(
+    name="note_append",
+    description="Append to an existing note",
+    guild_ids=[channels]
+)
+@option(
+    "message_id",
+    description="message_id (first line of message)"
+)
+@option(
+    "content",
+    description="content to append"
+)
 async def note_append(ctx):
-    await ctx.respond("note should be edited")
+    await ctx.respond("note should be appended")
 
-async def note_delete_lines(ctx):
-    await ctx.respond("note should have lines deleted")
 
+##COMMAND TO BE IMPLEMENTED LATER
+# @bot.slash_command(
+#     name="note_delete_lines",
+#     description="Delete lines of a note",
+#     guild_ids=[channels]
+# )
+# @option(
+#     "message_id",
+#     description="message_id (first line of message)"
+# )
+# @option(
+#     "line_number",
+#     description="line number to delete"
+# )
+# async def note_delete_lines(ctx):
+#     message = await ctx.respond("note should have lines deleted")
+#     print(f"message sent as {message.id}")
+
+
+@bot.slash_command(
+    name="note_delete",
+    description="Delete a note",
+    guild_ids=[channels]
+)
+@option(
+    "message_id",
+    description="message_id (first line of message)"
+)
 async def note_delete(ctx):
+    ## CHECK HERE RUNNING COMMAND USER IS SAME AS OWNER USER (OR ADMIN IF THAT CAN BE DETERMINED)
     await ctx.respond("note should be deleted")
 
 #MAIN
